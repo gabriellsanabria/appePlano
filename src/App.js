53a3f984b5da8326cb72e983da7df68f5d8695e6
gabriellsanabria <gabriellsanabria@gmail.com>
2024-04-22 21:08:17 -0300

    Pagina Manutenção

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Routes';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
+  // Estado de manutenção
+  const [manutencao, setManutencao] = useState(false);
+
+  // Função para alternar o estado de manutenção
+  const toggleManutencao = () => {
+    setManutencao(!manutencao);
+  };
+
   return (
     <Router>
-      <AppRoutes />
+      {!manutencao ? (
+        <div>
+          <NotFound />
+        </div>
+      ) : (
+        <AppRoutes />
+      )}
     </Router>
   );
 };
