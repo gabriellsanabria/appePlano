35673478f344030ae582e570bfab671ba1816e50
gabriellsanabria <gabriellsanabria@gmail.com>
2024-06-21 09:46:16 -0300

    alteração api para dominio

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,8 +2,6 @@
 const API_BASE_URL_AMPLIFY = 'http://127.0.0.1:5000';
 
 // Para o ambiente do Amplify
-const API_BASE_URL = 'https://54.221.22.223';
-
-// const API_BASE_URL = 'https://api.eplano.com.br';
+const API_BASE_URL = 'https://api.eplano.com.br'; // Supondo que esta seja a URL do seu endpoint no Amplify
 
 export { API_BASE_URL, API_BASE_URL_AMPLIFY };
