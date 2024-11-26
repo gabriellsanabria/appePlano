120938ace97db1f1528ffe655247b97dcdf2b08f
Gabriel Sanabria <gabriellsanabria@gmail.com>
2024-11-26 10:34:51 -0300

    ajuste texto

--- src/components/SobreComp/SobreComp.js
+++ src/components/SobreComp/SobreComp.js
@@ -27,18 +27,24 @@ function SobreComp() {
         Por que <span className='dstq'>e</span>Plano? Porque <span className='dstq'>o seu Negócio é uma ORGANIZAÇÃO VIVA</span>, que está em constante mudança!
         </h1>
       </div>
+      <div className='texto-teste'>
+        <p>
+          Se o desafio é um mundo em constante mudança, onde tudo parece estar mais rápido, as Empresas precisam desenvolver PLANOS mais ÁGEIS!
+          E isso só é possível se for feito com SIMPLICIDADE, AGILIDADE E INOVAÇÃO.
+        </p>
+        <p>
+            <strong>PLANOS mais Ágeis = Organização (para Operar melhor o seu Negócio).</strong>
+        </p>
+        <p>
+            Temos que PLANEJAR e REPLANEJAR mais rápido, você concorda?
+            E se você nunca estudou, leu e ou fez um Planejamento, FIQUE TRANQUILO; a gente preparou tudo para você.</p>
+        <p>
+            O ePlano é a solução que desenvolvemos ao longo de 10 anos para facilitar este progresso (processo, crescimento).
+            Elaboramos e desenhamos o Planejamento de centenas de Negócios, sempre buscando simplificar as Ferramentas de Planejamento e Gestão.
+        </p>
+      </div>
       <div className='w-text-content'>
-        <p>
-        Se o desafio é um mundo em constante mudança, onde tudo parece estar mais rápido, as Empresas precisam desenvolver PLANOS mais ÁGEIS!
-E isso só é possível se for feito com SIMPLICIDADE, AGILIDADE E INOVAÇÃO.
-        </p>
-        <p>
-          <strong>PLANOS mais Ágeis = Organização (para Operar melhor o seu Negócio).</strong>
-        </p>
-        <p>Temos que PLANEJAR e REPLANEJAR mais rápido, você concorda?
-E se você nunca estudou, leu e ou fez um Planejamento, FIQUE TRANQUILO; a gente preparou tudo para você.</p>
-<p>O ePlano é a solução que desenvolvemos ao longo de 10 anos para facilitar este progresso (processo, crescimento).
-Elaboramos e desenhamos o Planejamento de centenas de Negócios, sempre buscando simplificar as Ferramentas de Planejamento e Gestão.</p>
+        
         <h2>Somos a primeira Plataforma - Simples, Ágil e 100% online - desenvolvida para Planejar e Administrar Micro e Pequenos Negócios.</h2>
       </div>
 
